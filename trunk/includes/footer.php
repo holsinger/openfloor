63db91e82c98dff6cd26e5106cf7650181f8094a
daniel holsinger <holsinger@gmail.com>
2007-06-03 22:02:14 +0000

    basic page assets and layout

---
+++ trunk/includes/footer.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+        </div>
+  		</div>
+  		
+    </div>
+		<!-- footer start here -->
+		<div id="footer">
+			<p>Copyright &copy; 2007Politic20.com. All Rights Reserved. Reproduction in whole or part in any form or medium without express written permission is strictly prohibited. </p>
+		</div>
+	</div></body>
+</html>
---
+++ trunk/includes/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+	
+		
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<style media="all" type="text/css">@import "css/all.css";</style>
+	<!--[if lt IE 7]><link rel="stylesheet" type="text/css" href="css/lt7.css" media="screen"/><![endif]-->
+
+		
+	<title>Politic 2.0</title>
+	<link rel="icon" href="/p20/favicon.ico" type="image/x-icon"/>
+</head>
+
+<body>
+			<div id="page">
+		<!-- header start here -->
+		<div id="header">
+			<h1><a href="http://localhost/p20">Politic20</a></h1>
+			<form action="http://localhost/p20">
+				<div>
+					<input type="text" class="txt" />
+					<input type="image" src="images/btn-search.gif" alt="search" />
+
+				</div>
+			</form>
+		</div>
+		<!-- top navigation start here -->
+		<div class="body">
+      <ul class="top-nav">
+  			<li><span><a href="#">Home</a></span></li>
+  			<li><a href="#">About Us</a></li>
+  			<li><a href="#">Blog</a></li>
+  
+  		</ul>
+  		<div class="slogan"><strong class="slogan">populace politic change</strong></div>
+  		
+  		<div id="pagewidth">
+  			<div class="frame">
