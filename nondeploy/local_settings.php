ecf8ccfac17796da09998045a4376c5ed7205325
daniel holsinger <holsinger@gmail.com>
2007-06-05 07:28:52 +0000

    this file should be unique to a server install

---
+++ nondeploy/local_settings.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Created on Jun 5, 2007
+ *
+ * @author James Kleinschnitz
+ */
+
+
+// define current working directory
+define ("ENFORCE_SECURITY_LEVELS", false);
+define ("CONFIG_XML_FILE", "/php5/htdocs/p20/trunk/conf/p20_conf.xml");
+define ("DATABASE_XML_FILE", "/php5/htdocs/p20/trunk/conf/p20_table_conf.xml");
+define ("ADMIN_EMAIL", "james@engeb.com");
+define ("LOG_FILE", "/var/www/logs/php_app_log");
+?>
