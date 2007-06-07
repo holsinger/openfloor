bfcd4da9bc2d5c58845f18d2cf6dd421034af9a4
daniel holsinger <holsinger@gmail.com>
2007-06-07 19:40:44 +0000

    cleaned up dal for query only

--- trunk/apps/admin_session.php
+++ trunk/apps/admin_session.php
@@ -6,7 +6,6 @@ define ("APPS_ROOT", dirname(__FILE__));
 
 // set error reporting level globally
 error_reporting(E_WARNING | E_ERROR);
-//error_reporting(E_WARNING | E_ERROR);
 
 // initialize sessions normally
 $first_visit;
@@ -18,10 +17,7 @@ session_start ();
 // you may want to customize these includes
 require_once(APPS_ROOT."/local_settings.php");
 require_once(APPS_ROOT."/db/db_connection.php");
-require_once(APPS_ROOT."/db/db_table.php");
 require_once(APPS_ROOT."/db/db_mysql.php");
-require_once(APPS_ROOT."/db/db_sql_utility.php");
-require_once(APPS_ROOT."/db/db_vsql_utility.php");
 
 //special apps
 require_once(APPS_ROOT."/api_data.php");
@@ -30,5 +26,6 @@ require_once(APPS_ROOT.'/../dal/dal.php');
 require_once(APPS_ROOT.'/global/global.php');
 
 $db =& new db_connection();
+
 
 // consider including global authorization code here
