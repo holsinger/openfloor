8ca048a17121c5da31df319c2ae73fdeb742625b
daniel holsinger <holsinger@gmail.com>
2007-06-19 17:27:11 +0000

    session setup files

---
+++ trunk/apps/convention_sessions.php
@@ -0,0 +1,35 @@
+<?
+
+// define current working directory
+define ("APPS_ROOT", dirname(__FILE__));
+//define ("ENFORCE_SECURITY_LEVELS", false);
+
+// set error reporting level globally
+error_reporting(E_WARNING | E_ERROR);
+
+// initialize sessions normally
+$first_visit = false;
+if (!isset($_COOKIE['PHPSESSID']))
+	$first_visit = true;
+
+session_start ();
+
+// you may want to customize these includes
+require_once(APPS_ROOT."/local_settings.php");
+require_once(APPS_ROOT."/db/db_connection.php");
+require_once(APPS_ROOT."/db/db_mysql.php");
+require_once(APPS_ROOT.'/dbfunctions.php');
+require_once(APPS_ROOT.'/Guid.php');
+require_once(APPS_ROOT.'/Visit.php');
+require_once(APPS_ROOT.'/Zip.php');
+
+$db = new db_connection();
+//special apps
+require_once(APPS_ROOT."/api_data.php");
+require_once(APPS_ROOT.'/../dal/dal.php');
+require_once(APPS_ROOT.'/global/global.php');
+
+//$db = new db_connection();
+
+
+// consider including global authorization code here
