6a9ea18bcdd5f00f7097ca2115a0cff5d124f122
daniel holsinger <holsinger@gmail.com>
2007-06-20 08:55:40 +0000

    change config options

--- trunk/system/application/config/config.php
+++ trunk/system/application/config/config.php
@@ -11,7 +11,7 @@
 |	http://www.your-site.com/
 |
 */
-$config['base_url']	= "http://127.0.0.1/CodeIgniter/";
+$config['base_url']	= "http://127.0.0.1/p20/";
 
 /*
 |--------------------------------------------------------------------------
@@ -222,8 +222,8 @@ $config['encryption_key'] = "";
 */
 $config['sess_cookie_name']		= 'ci_session';
 $config['sess_expiration']		= 7200;
-$config['sess_encrypt_cookie']	= FALSE;
-$config['sess_use_database']	= FALSE;
+$config['sess_encrypt_cookie']	= TRUE;
+$config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
@@ -251,7 +251,7 @@ $config['cookie_path']		= "/";
 | COOKIE data is encountered
 |
 */
-$config['global_xss_filtering'] = FALSE;
+$config['global_xss_filtering'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
