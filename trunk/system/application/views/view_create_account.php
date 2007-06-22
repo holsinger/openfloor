97dd09156589d28e214cac086f7a0387498e16f5
daniel holsinger <holsinger@gmail.com>
2007-06-22 01:11:14 +0000

    move captcha to controlller

--- trunk/system/application/views/view_create_account.php
+++ trunk/system/application/views/view_create_account.php
@@ -1,18 +1,30 @@
 <? $this->load->view('view_includes/header.php'); ?>
 
 <div id="content_div">
-  <h3>Create Account</h3>	
+  <h2>Create Account</h2>	
   	<div class='errorArea'><?=$error;?></div>
 	<div id="account_form">
 	<?= form_open('user/create'); ?>
 	
-	<?= form_format("Username: ",form_input('user_name','','class="txt"') ); ?>
-	<?= form_format("Password: ",form_password('user_password','','class="txt"') ); ?>
-	<?= form_format("Upload an Avatar: ",form_upload('user_avatar','','class="txt"') ); ?>
-	<?= form_format("Display Name: ",form_input('user_display_name','','class="txt"') ); ?>
+	<?= form_format("Username: *",form_input('user_name',$this->validation->user_name,'class="txt"') ); ?>
+	<?= form_format("Password: *",form_password('user_password','','class="txt"') ); ?>
+	<?= form_format("Password Confirm: *",form_password('password_confirm','','class="txt"') ); ?>
+	<?= form_format("Email: *",form_input('user_email',$this->validation->user_email,'class="txt"') ); ?>
+	
+	
 	<br /><br />
-	<?= form_submit('','Create Account'); ?>
+	<?		
+	echo $capimage;
+	echo '<br />';
+	echo '<label>Enter the above characters: *</label>';
+	echo form_input('captcha','','class="txt"')
+	?>
+	<br /><br />
+	<?= form_submit('','Create Account','class="button"'); ?>
+	<br /><br />
+	<small>* required fields</small>
 	<?= form_close(); ?>
 	</div>
+	
 </div>
 <? $this->load->view('view_includes/footer.php'); ?>  				
