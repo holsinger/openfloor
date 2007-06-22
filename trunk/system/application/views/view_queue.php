0a4af0b79712eb883069b1ad2423b3e5d7a99675
daniel holsinger <holsinger@gmail.com>
2007-06-22 01:11:53 +0000

    use the pod view

---
+++ trunk/system/application/views/view_queue.php
@@ -0,0 +1,11 @@
+<?$this->load->view('view_includes/header.php');?>
+<div id="content_div">
+	<h2>Questions</h2>
+	<div id='queue'>
+	<?
+	foreach ($results as $row)
+		$this->load->view('view_includes/view_question_pod.php',$row);
+	?>
+	</div>	
+</div>
+<?$this->load->view('view_includes/footer.php');?>
