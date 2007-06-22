cdfd4041f73d909ed4c5e489ba15411b86c06b7a
daniel holsinger <holsinger@gmail.com>
2007-06-22 01:11:38 +0000

    add redirect option

--- trunk/system/application/views/view_login.php
+++ trunk/system/application/views/view_login.php
@@ -1,21 +1,24 @@
 <? $this->load->view('view_includes/header.php'); ?>
 
 <div id="content_div">
-  <h3>Login</h3>	
+  <h2>Login</h2>	
   	<div class='errorArea'><?=$error;?></div>
+  	<br />
 	<div id="login_form">
 	<?= form_open('user/login'); ?>
-	<?= form_format("Username: ",form_input('user_name','','class="txt"') ); ?>
+	<?= form_format("Username: ",form_input('user_name',$this->validation->user_name,'class="txt"') ); ?>
 	<?= form_format("Password: ",form_password('user_password','','class="txt"') ); ?>
-	<br /><?= form_submit('','Login'); ?>
+	<br /><br /><?= form_submit('','Login','class="button"'); ?>
 	<?= form_close(); ?>
 	</div>
 	<br /><br />
 	<?= form_open('user/loginOpenID'); ?>
 	<?= form_format("OpenID Login: ",form_input('openid_url','','class="txt"') ); ?>
 	<?= form_hidden('openid_action','login'); ?>
-	<br /><?= form_submit('','Login'); ?>
-	<?= form_close(); ?>
+	<br />
 	<div><a href="http://www.myopenid.com/" class="link" target='_top'>Get an OpenID</a></div>
+	<br /><?= form_submit('','Login','class="button"'); ?>
+	<?= form_close(); ?>
+	
 </div>
 <? $this->load->view('view_includes/footer.php'); ?>  				
