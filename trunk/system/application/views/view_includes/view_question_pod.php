ee07ae782cc185cc5120979697a11f182f9d79fc
daniel holsinger <holsinger@gmail.com>
2007-06-26 17:25:03 +0000

    styling

--- trunk/system/application/views/view_includes/view_question_pod.php
+++ trunk/system/application/views/view_includes/view_question_pod.php
@@ -1,11 +1,39 @@
-<p><table width="70%" cellspacing="0" cellpadding="3">
-	<tr><td bgcolor="#0066cc" width='175'>
-			<table>
-				<tr><td><a href="index.php/question/voteup/<?= $question_id; ?>">[+]</a></td><td rowspan='2' align='center'><?=$votes;?><br/>votes</td></tr>
-				<tr><td><a href="index.php/question/votedown/<?= $question_id; ?>">[-]</a></td></tr>
-			</table>
-		</td>
-		<td bgcolor="#99ccff"><b><?=$question_name;?></b></td></tr>
-	<tr><td bgcolor="#99ccff">Event:<br/><?=$event_name;?></td><td bgcolor="#ccffff"><img src='http://localhost/~Rob/Politic20/images/rob.jpg'/>Posted By: <?=$user_name;?></td></tr>
-	<tr><td bgcolor="#99ccff"><img src='images/meet_mitt_romney.jpg' width='52' height='62'/></td><td bgcolor="#ccffff"><?=$question_desc;?></td></tr>
-</table></p>
+
+<div class="news-summary" id="xnews-5">
+	<!-- raiting topics start here -->
+	<div class="raiting" >
+		<span id="xvote-5" class="next_invisible">
+			<a href="index.php/question/voteup/<?= $question_id; ?>" class="up">up</a>	
+		</span>
+		<span id="xreport-5">
+			<a href="index.php/question/votedown/<?= $question_id; ?>" class="down">down</a>
+		</span>
+		<a id="xvotes-5" href="/story.php?title=Gay-Marriage-How-do-you-propose-to-offer-balance-with-gay-community#who_voted" class="vote_digit"><?=$votes;?></a>
+	</div>
+																					
+	<div class="describtion">
+		<div class="describtion-frame">
+			<div class="descr-tr">
+				<div class="descr-bl">
+					<div class="descr-br">
+						<h3><a href="/story.php?title=Gay-Marriage-How-do-you-propose-to-offer-balance-with-gay-community"><?=$question_name;?></a></h3>
+						<div class="autor">
+							<p>Posted by: <a href="/user.php?login=paulwilson"><?=$user_name;?></a> 72 days ago
+								<span id="ls_story_link-5">(Question)</span>
+							</p>
+							<p>
+								Event: <a href="/index.php?category=ChrisCannon04%2F07"><?=$event_name;?></a><span id="ls_adminlinks-5" style="display:none"></span>
+							</p>
+						</div>
+						<p><?=$question_desc;?><a href="/story.php?title=Gay-Marriage-How-do-you-propose-to-offer-balance-with-gay-community" class="more"> read more &raquo;</a></p>
+						<ul class="options">
+							<li class="discuss"><a href="/story.php?title=Gay-Marriage-How-do-you-propose-to-offer-balance-with-gay-community">Discuss</a></li> 	
+							<li class="tell-friend" id="ls_recommend-5"><a href="javascript://" onclick="show_recommend(5, 58, 'http://conventionnext.politic20.com/');">Tell a friend</a></li>
+						</ul>
+						<span id="emailto-5" style="display:none"></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
