3b5b554b666f72c833db49cd4c2883e1242bd624
daniel holsinger <holsinger@gmail.com>
2007-06-26 17:24:45 +0000

    styling

--- trunk/system/application/views/welcome_message.php
+++ trunk/system/application/views/welcome_message.php
@@ -2,9 +2,9 @@
 
 <div id="content_div">
             <h3>Welcome</h3>
-            <p style="margin-left:10px;margin-right:10px;">
+            <p style="margin-left:10px;margin-right:30px;">
             <span class='red'>Politic2.0</span> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi consequat pulvinar erat. Curabitur scelerisque, massa nec aliquam cursus, lacus metus accumsan risus, quis viverra tellus ligula non neque. Aliquam vel felis. Fusce mattis, quam eu rutrum malesuada, tortor tortor nonummy augue, nec malesuada velit neque id eros. Ut tortor. Ut at tortor a augue semper volutpat. Aenean nonummy egestas lacus. Donec sagittis, risus vel rutrum sollicitudin, velit ligula sollicitudin turpis, a fermentum libero est a ante. Pellentesque feugiat. Quisque ut est.</p>
-            <p style="margin-left:10px;margin-right:10px;">
+            <p style="margin-left:10px;margin-right:30px;">
             Donec quis libero. Duis mattis pulvinar nisl. Mauris et sem. Sed aliquet. Donec sit amet tellus. Praesent et lacus ac justo pellentesque viverra. Vivamus neque ante, fermentum eu, tempus sed, tincidunt a, pede. Quisque eu dolor. Donec elementum dapibus arcu. Phasellus porttitor consequat nisi. Etiam non tellus sed ligula dapibus mollis. Etiam eget dolor at massa commodo adipiscing. Morbi pretium pellentesque magna. Sed at elit. Ut gravida rhoncus augue. Integer imperdiet, ipsum nec imperdiet varius, odio leo porta ante, nec imperdiet pede nisl et dolor. Phasellus euismod placerat enim. Integer placerat faucibus urna. </p>
             
             <br><br>
