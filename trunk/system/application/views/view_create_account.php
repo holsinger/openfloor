887cec94d2fb5c9d8212abc7b0e9227374813471
daniel holsinger <holsinger@gmail.com>
2007-06-26 17:22:34 +0000

    open id

--- trunk/system/application/views/view_create_account.php
+++ trunk/system/application/views/view_create_account.php
@@ -4,12 +4,40 @@
   <h2>Create Account</h2>	
   	<div class='errorArea'><?=$error;?></div>
 	<div id="account_form">
-	<?= form_open('user/create'); ?>
 	
-	<?= form_format("Username: *",form_input('user_name',$this->validation->user_name,'class="txt"') ); ?>
-	<?= form_format("Password: *",form_password('user_password','','class="txt"') ); ?>
-	<?= form_format("Password Confirm: *",form_password('password_confirm','','class="txt"') ); ?>
-	<?= form_format("Email: *",form_input('user_email',$this->validation->user_email,'class="txt"') ); ?>
+
+	<? if ( isset($openID) ) { ?>
+		<br />
+		<?= form_open('user/create'); ?>
+		<strong><img src='images/openid-icon-small.gif'> OpenID:</strong> 
+		<?=$openID;?>
+		<br />
+		<?= form_hidden('user_openid',$openID); ?>
+	<? } else { ?>
+		<?= form_open('user/createOpenID'); ?>
+		<?= form_format("<img src='images/openid-icon-small.gif'> OpenID: ",form_input('openid_url','','class="txt"') ); ?>
+		<?= form_hidden('openid_action','create'); ?>
+		<br />
+		<div>
+			<a href="http://www.myopenid.com/" class="link" target='_top'>Get an OpenID</a>
+		</div>
+		<br /><?= form_submit('','Validate','class="button"'); ?>
+		<?= form_close(); ?>
+		<br /><br />
+		<?= form_open('user/create'); ?>
+	<? } ?>
+	
+	
+	<?= form_format("Username: *",form_input('user_name',(isset($this->validation->user_name))?$this->validation->user_name:'','class="txt"') ); ?>
+	<? if ( !isset($openID) ) echo form_format("Password: *",form_password('user_password','','class="txt"') ); ?>
+	<? if ( !isset($openID) ) echo form_format("Password Confirm: *",form_password('password_confirm','','class="txt"') ); ?>
+	
+	<?
+	if (isset($openID_email)) $email = $openID_email;
+	else if (isset($this->validation->user_email)) $email = $this->validation->user_email;
+	else $email= '';
+	?>
+	<?= form_format("Email: *",form_input('user_email',$email,'class="txt"') ); ?>
 	
 	
 	<br /><br />
--- trunk/system/application/views/view_login.php
+++ trunk/system/application/views/view_login.php
@@ -6,17 +6,19 @@
   	<br />
 	<div id="login_form">
 	<?= form_open('user/login'); ?>
-	<?= form_format("Username: ",form_input('user_name',$this->validation->user_name,'class="txt"') ); ?>
+	<?= form_format("Username: ",form_input('user_name',(isset($this->validation->user_name)) ? $this->validation->user_name:'','class="txt"') ); ?>
 	<?= form_format("Password: ",form_password('user_password','','class="txt"') ); ?>
 	<br /><br /><?= form_submit('','Login','class="button"'); ?>
 	<?= form_close(); ?>
 	</div>
 	<br /><br />
 	<?= form_open('user/loginOpenID'); ?>
-	<?= form_format("OpenID Login: ",form_input('openid_url','','class="txt"') ); ?>
+	<?= form_format("<img src='images/openid-icon-small.gif'> OpenID Login: ",form_input('openid_url','','class="txt"') ); ?>
 	<?= form_hidden('openid_action','login'); ?>
 	<br />
-	<div><a href="http://www.myopenid.com/" class="link" target='_top'>Get an OpenID</a></div>
+	<div>
+		<a href="http://www.myopenid.com/" class="link" target='_top'>Get an OpenID</a>
+	</div>
 	<br /><?= form_submit('','Login','class="button"'); ?>
 	<?= form_close(); ?>
 	
