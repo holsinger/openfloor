06a34872792b7f950d4b5a2179741541b16be6e2
daniel holsinger <holsinger@gmail.com>
2007-06-28 03:48:25 +0000

    more js for some effects

--- trunk/system/application/views/view_includes/view_head_setup.php
+++ trunk/system/application/views/view_includes/view_head_setup.php
@@ -8,6 +8,8 @@
 	<!--[if lt IE 7]><link rel="stylesheet" type="text/css" href="css/lt7.css" media="screen"/><![endif]-->
 	
 	<script type="text/javascript" src="javascript/prototype.js"></script>
+	<script src="javascript/scriptaculous.js" type="text/javascript"></script>
+	<script src="javascript/effects.js" type="text/javascript"></script>
 	<script type="text/javascript" src="javascript/userWindow.js"></script>
 	<?php if(isset($this->validation->event_date)) echo @js_calendar_script('my_form');  ?>
 	<title>Politic 2.0</title>
