8a68e6788c8ec14dab828c6da1a4cfe9fee447d2
daniel holsinger <holsinger@gmail.com>
2007-06-28 03:46:39 +0000

    better event view format

--- trunk/system/application/views/view_events.php
+++ trunk/system/application/views/view_events.php
@@ -1,13 +1,22 @@
 <? $this->load->view('view_includes/header.php'); ?>
 
 <div id="content_div">
-  <h2>Manage Events</h2>	
+  <h2>View Events</h2>	
   	<div id="account_form">
 
-	<p><?=anchor('/event/create_event','Create an event')?>
+	<p><? if ($this->userauth->isAdmin()) echo anchor('/event/create_event','Create an event');?>
 
-	<?=$this->table->generate($events)?>
-	
+	<? //echo $this->table->generate($events)?>
+	<? foreach ($events as $key => $array) {?>
+		<div id='event<?=$array['event_id'];?>' class='event-summary'>
+		<br /><strong><?=$array['event_name'];?></strong>
+		<span style"float:right;"><?=$array['event_avatar'];?></span>
+		<br /><b>When:</b> <?=$array['event_date'];?>
+		<br /><b>Where:</b> <?=$array['location'];?>
+		<br /><?=$array['event_desc'];?>
+		<? if ($this->userauth->isAdmin()) echo "<br />".$array['edit'];?>
+		</div>
+	<? }?>
 	</div>
 
 </div>
