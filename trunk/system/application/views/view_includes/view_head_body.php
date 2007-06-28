2e4c41239f6905ce3809c23c0722a9988e11348b
daniel holsinger <holsinger@gmail.com>
2007-06-28 03:47:53 +0000

    link to events login change

--- trunk/system/application/views/view_includes/view_head_body.php
+++ trunk/system/application/views/view_includes/view_head_body.php
@@ -6,15 +6,14 @@
 		<div id="header">
 			<h1><a href="http://www.politic20.com">Politic20</a></h1>
 			<? if ($this->session->userdata('user_name')) { ?>
-			<div id="userLogin"><br />welcome <a href="index.php/user/profile/<?=$this->session->userdata('user_name');?>"><?=$this->session->userdata('user_name');?></a>&nbsp;|&nbsp;<a href="index.php/user/logout/">Logout</a></div>
+			<div id="userLogin">Welcome, <a href="index.php/user/profile/<?=$this->session->userdata('user_name');?>"><?=$this->session->userdata('user_name');?></a>&nbsp;|&nbsp;<a href="index.php/user/profile/<?=$this->session->userdata('user_name');?>">Manage Account</a>&nbsp;|&nbsp;<a href="index.php/user/logout/">Logout</a></div>
 			<? } else { ?>
-			<div id="userLogin">Customize your P20 experience<br /> <span onClick="showBox('login');">Login</span>&nbsp;|&nbsp;<a href="index.php/user/createAccount/">Create Account</a></div>
+			<div id="userLogin"><span onClick="showBox('login');">Login</span>&nbsp;|&nbsp;<a href="index.php/user/createAccount/">Create Account</a></div>
 			<? } ?>
 			<form action="whosyourgovt.php" method="get">
 				<div>
 					<input type="text" class="txt" name="zip" value="<?= isset($_GET['zip'])?$_GET['zip']:'' ?>"/>
 					<input type="image" src="images/btn-go.gif" alt="search" />
-
 				</div>
 			</form>
 		</div>
@@ -22,7 +21,7 @@
 		<div class="body">
       <ul class="top-nav">
   			<li><span><a href="http://www.politic20.com">Home</a></span></li>
-  			<li><a href="#">Events</a></li>
+  			<li><a href="index.php/event/">Events</a></li>
   			<li><a href="#">About Us</a></li>
   			<li><a href="http://blog.politic20.com">Blog</a></li>
   
