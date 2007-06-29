cfa0174d6d16288828d6ed324553a5831a4cc5ca
daniel holsinger <holsinger@gmail.com>
2007-06-29 03:43:29 +0000

    link changes

--- trunk/system/application/views/view_includes/view_question_pod.php
+++ trunk/system/application/views/view_includes/view_question_pod.php
@@ -5,10 +5,10 @@
 		<span id="xvote-5" class="next_invisible">
 			<a href="index.php/question/voteup/<?= $question_id; ?>" class="up">up</a>	
 		</span>
-		<span id="xreport-5">
+		<span id="xreport-<?= $question_id; ?>">
 			<a href="index.php/question/votedown/<?= $question_id; ?>" class="down">down</a>
 		</span>
-		<a id="xvotes-5" href="/story.php?title=Gay-Marriage-How-do-you-propose-to-offer-balance-with-gay-community#who_voted" class="vote_digit"><?=$votes;?></a>
+		<a id="xvotes-<?= $question_id; ?>" href="index.php/votes/who/<?= $question_id; ?>" class="vote_digit"><?=(is_numeric($votes))?$votes:0;?></a>
 	</div>
 																					
 	<div class="describtion">
@@ -16,19 +16,19 @@
 			<div class="descr-tr">
 				<div class="descr-bl">
 					<div class="descr-br">
-						<h3><a href="/story.php?title=Gay-Marriage-How-do-you-propose-to-offer-balance-with-gay-community"><?=$question_name;?></a></h3>
+						<h3><a href="index.php/question/queue/event/<?= url_title($question_name); ?>"><?=$question_name;?></a></h3>
 						<div class="autor">
-							<p>Posted by: <a href="/user.php?login=paulwilson"><?=$user_name;?></a> 72 days ago
-								<span id="ls_story_link-5">(Question)</span>
+							<p>Posted by: <a href="/user/profile/<?=$user_name;?>"><?=$user_name;?></a> <!-- 72 days ago -->
+								<span id="ls_story_link-<?= $question_id; ?>"></span>
 							</p>
 							<p>
-								Event: <a href="/index.php?category=ChrisCannon04%2F07"><?=$event_name;?></a><span id="ls_adminlinks-5" style="display:none"></span>
+								Event: <a href="index.php/event"><?=$event_name;?></a><span id="ls_adminlinks-5" style="display:none"></span>
 							</p>
 						</div>
-						<p><?=$question_desc;?><a href="/story.php?title=Gay-Marriage-How-do-you-propose-to-offer-balance-with-gay-community" class="more"> read more &raquo;</a></p>
+						<p><?=$question_desc;?><a href="index.php/question/queue/event/<?= url_title($question_name); ?>" class="more"> read more &raquo;</a></p>
 						<ul class="options">
-							<li class="discuss"><a href="/story.php?title=Gay-Marriage-How-do-you-propose-to-offer-balance-with-gay-community">Discuss</a></li> 	
-							<li class="tell-friend" id="ls_recommend-5"><a href="javascript://" onclick="show_recommend(5, 58, 'http://conventionnext.politic20.com/');">Tell a friend</a></li>
+							<li class="discuss"><a href="index.php/question/queue/event/<?= url_title($question_name); ?>">Discuss</a></li> 	
+							<li class="tell-friend" id="ls_recommend-5"><a href="javascript://" onclick="show_recommend(5, 58, '<?= $this->config->site_url();?>');">Tell a friend</a></li>
 						</ul>
 						<span id="emailto-5" style="display:none"></span>
 					</div>
