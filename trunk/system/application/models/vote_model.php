029c9d44afe0187a9ddec60eb10c78c2033e4662
daniel holsinger <holsinger@gmail.com>
2007-07-13 05:54:48 +0000

    move vote functions to vote model

--- trunk/system/application/models/vote_model.php
+++ trunk/system/application/models/vote_model.php
@@ -27,5 +27,16 @@ class Vote_model extends Model
 		return ($this->db->query("SELECT vote_id FROM cn_votes WHERE fk_user_id=$user_id AND fk_question_id=$question_id")->num_rows() > 0) ? true : false ;		
 	}
 	
+	public function votedScore($question_id, $user_id)
+	{
+		$query = $this->db->query("SELECT vote_value FROM cn_votes WHERE fk_user_id=$user_id AND fk_question_id=$question_id order by timestamp desc");
+		$voted = ($query ->num_rows() > 0) ? true : false ;
+		if ($voted) 
+		{
+			$array = $query->result_array();
+			return $array[0]['vote_value'];
+		}
+		else return $voted;		
+	}
 }
 ?>
