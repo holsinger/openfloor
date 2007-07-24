e312ae52eb99733900b41668e0a1631feb3f92c7
daniel holsinger <holsinger@gmail.com>
2007-07-24 05:43:25 +0000

    use for dynamic pop-up view

--- trunk/javascript/userWindow.js
+++ trunk/javascript/userWindow.js
@@ -1,3 +1,6 @@
+var my_width  = 0;
+var my_height = 0;
+    
 function showBox(id){
     $('overlay').show();
     center(id);
@@ -10,15 +13,7 @@ function hideBox(id){
     return false;
 }
 
-function center(element){
-    try{
-        element = $(element);
-    }catch(e){
-        return;
-    }
-
-    var my_width  = 0;
-    var my_height = 0;
+function center(string){     
 
     if ( typeof( window.innerWidth ) == 'number' ){
         my_width  = window.innerWidth;
@@ -34,11 +29,51 @@ function center(element){
         my_width  = document.body.clientWidth;
         my_height = document.body.clientHeight;
     }
+		
+		//see if we have a div
+		element = $(string);
+		if (!element) {
+			element = getDynamicElement(string);	
+		} else {
+	    //set element styles
+	    element.style.position = 'absolute';
+	    element.style.zIndex   = 1000;
+	    cleanUp (element);
+	    new Effect.Appear(string);
+	  }
+	    
+}
+function getDynamicElement(string) {
+	var my_div = $('hijax');
+	Element.extend(my_div);
+	//set element styles
+  my_div.style.position = 'absolute';
+  my_div.style.zIndex   = 1000;
+	my_div.innerHTML = 'Loading...';
+	my_div.show();
+	new Ajax.Request(site_url+'/information/viewAjax/'+string,
+  {
+    method:'post',
+    onSuccess: function(transport){
+      var response = transport.responseText;
+      //console.debug(response);
+      			
+			my_div.addClassName('ajax_box');
+			my_div.hide();
+			my_div.innerHTML = '<img id="close" src="images/close.gif" onclick="hideBox(\'hijax\')" alt="Close" title="Close this window" />'+response;
+			
+			cleanUp (my_div);			
+			my_div.style.display  = 'none';
+			new Effect.Appear('hijax');		
+			   
+	    
+    },
+    onFailure: function(){ alert('Something went wrong...') }
+  });
+}
 
-    element.style.position = 'absolute';
-    element.style.zIndex   = 99;
-
-    var scrollY = 0;
+function cleanUp (element) {
+		var scrollY = 0;
 
     if ( document.documentElement && document.documentElement.scrollTop ){
         scrollY = document.documentElement.scrollTop;
@@ -49,8 +84,8 @@ function center(element){
     }else if ( window.scrollY ){
         scrollY = window.scrollY;
     }
-
-    var elementDimensions = Element.getDimensions(element);
+    
+		var elementDimensions = Element.getDimensions(element);
 
     var setX = ( my_width  - elementDimensions.width  ) / 2;
     var setY = ( my_height - elementDimensions.height ) / 2 + scrollY;
@@ -61,5 +96,5 @@ function center(element){
     element.style.left = setX + "px";
     element.style.top  = setY + "px";
 
-    element.style.display  = 'block';
+    element.style.display  = 'block';    
 }
