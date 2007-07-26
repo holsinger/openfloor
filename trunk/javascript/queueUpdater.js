a7deec7c14e341308ccd49e1b2dac458429b584a
daniel holsinger <holsinger@gmail.com>
2007-07-26 20:25:57 +0000

    added check for logged in on vote

--- trunk/javascript/queueUpdater.js
+++ trunk/javascript/queueUpdater.js
@@ -13,13 +13,20 @@ queueUpdater.updateQueue = function() {
 
 queueUpdater.updateQueueOnce = function() {
 	new Ajax.Updater('queue', site_url + '/conventionnext/ajQueueUpdater' + window.location.pathname);
+	//new Effect.SlideDown ('queue');
 }
 
-queueUpdater.vote = function(url) {
+
+queueUpdater.vote = function(url,id) {	
+	//new Effect.DropOut ('queue');
+	if (username.length <= 0) {
+		showBox('login');
+		return;
+	}
+	new Effect.Opacity (id,{duration:.5, from:1.0, to:0.7});
 	new Ajax.Request(url, {
 	  onSuccess: function(transport) {
 		  queueUpdater.updateQueueOnce();
-	      console.log('successfully voted!');
 	  }
 	});
 }
