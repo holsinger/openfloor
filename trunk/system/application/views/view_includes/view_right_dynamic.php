ed2840e4c7c3b8aa55cd23bee33894fd7fde95bd
daniel holsinger <holsinger@gmail.com>
2007-08-01 18:14:35 +0000

    add inner box styling for right pod padding

--- trunk/system/application/views/view_includes/view_right_dynamic.php
+++ trunk/system/application/views/view_includes/view_right_dynamic.php
@@ -2,8 +2,10 @@
 foreach ($dynamic as $key => $data) {
 ?>
 <h3><a class="close" onclick="new Effect.toggle('<?=$key;?>','blind', {queue: 'end'}); ">close</a><span><?=ucwords(str_replace('_',' ',$key));?></span></h3>
-<div class="box" id="<?=$key;?>">
+<div class="box">
+<div class="inner-box" id="<?=$key;?>">
  <span style="padding-left:5px;"><?=$data;?></span>
+</div>
 </div>
 <div class="box-bottom"></div> 
 <? } ?>
