629e5d9da3ad0e2ea1d765fda7b63275e614de13
daniel holsinger <holsinger@gmail.com>
2007-08-04 06:06:23 +0000

    make sure we show event data

--- trunk/system/application/controllers/conventionnext.php
+++ trunk/system/application/controllers/conventionnext.php
@@ -241,7 +241,7 @@ class Conventionnext extends Controller
 
 			if(empty($data['results'])) {
 				$event['results'] = $this->event->get_event($event_id);
-				$data['rightpods'] = array('dynamic'=>array('event_description'=>$event['results'][0]['event_desc'],'event_location'=>$event['results'][0]['location']));
+				$data['rightpods'] = array('dynamic'=>array('event_description'=>$event['results']['event_desc'],'event_location'=>$event['results']['location']));
 			} else {
 				$data['rightpods'] = array('dynamic'=>array('event_description'=>$data['results'][0]['event_desc'],'event_location'=>$data['results'][0]['location']));
 			}
