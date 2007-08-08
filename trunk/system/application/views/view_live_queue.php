187f7605e1593376c6c968672cdaffaaef63f628
daniel holsinger <holsinger@gmail.com>
2007-08-08 03:19:42 +0000

    creative for live queue

--- trunk/system/application/views/view_live_queue.php
+++ trunk/system/application/views/view_live_queue.php
@@ -8,31 +8,35 @@
 	<title>view_live_queue</title>
 	<meta name="generator" content="TextMate http://macromates.com/">
 	<meta name="author" content="Rob Stefanussen">
+	<link rel="stylesheet" type="text/css" href="css/all.css" />
 	<link rel="stylesheet" type="text/css" href="css/view_live_queue.css" />
 	<script type="text/javascript" src="javascript/lib/prototype.js"></script>
+	<script src="javascript/src/scriptaculous.js" type="text/javascript"></script>
 	<script type="text/javascript">site_url = '<?= $this->config->site_url();?>';var event_name = '<?=$event?>';</script>
-	<script type="text/javascript" src="javascript/liveQueueUpdater.js"></script>
+
+<script type="text/javascript" src="javascript/liveQueueUpdater.js"></script>
 	<script type="text/javascript">liveQueueUpdater.liveQueueUpdate();</script>
 	<!-- Date: 2007-08-02 -->
 </head>
 <body>
-
-<h3>Current Question</h3>	
-<? if(!empty($current_question)): ?>	
-<div id="current_question" class="current_question"><p>
-<? echo $current_question[0]['question_name']; ?>
-</p></div>
-<? else: ?>
-<div class="no_current_question"><p>There is no current question</p></div>
-<? endif; ?>
-<h3>Upcoming Questions:</h3>
-<div id="upcoming_questions" class="upcoming_questions">
-<?	foreach($questions as $question): 
-		$votes = ($question['votes'] == 1) ? 'vote' : 'votes' ;
-		echo "<p><span class=\"votes\">{$question['votes']} $votes</span>";
-		echo "<span class=\"question\">{$question['question_name']}</span></p>";
-	endforeach;	?>
-</div>
-
+		<br />
+		<span class='queue-logo'>
+			<img src="./images/bg-convention-next.gif" alt="convention-next"/>
+		</span>
+		<br />
+		<ul class="top-nav"></ul>
+		
+		<h3> &nbsp;&nbsp;Current Question</h3>	
+		<div id="current_question" class="current_question">
+		<? echo (!empty($current_question)) ? $current_question[0]['question_name']:'There is no current question'; ?>
+		</div>
+		<h3> &nbsp;&nbsp;Upcoming Questions:</h3>
+		<div id="upcoming_questions" class="upcoming_questions">
+		<?	foreach($questions as $question): 
+				$votes = ($question['votes'] == 1) ? 'vote ' : 'votes' ;
+				echo "<div class='queue-question'><span class=\"votes\"><p>{$question['votes']} $votes<p></span>";
+				echo "<span class=\"question\">{$question['question_name']}</span></div><br />";
+			endforeach;	?>
+		</div>
 </body>
 </html>
