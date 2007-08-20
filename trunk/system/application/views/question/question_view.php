ed0f9ced05827c75482a736fb4f5a9523b5c4148
daniel holsinger <holsinger@gmail.com>
2007-08-20 17:30:31 +0000

    Re added 'event_url'

--- trunk/system/application/views/question/question_view.php
+++ trunk/system/application/views/question/question_view.php
@@ -2,6 +2,7 @@
 $data['red_head'] = $event_type;
 $data['tabs'] = $event_type;
 $data['tab_view_question'] = 'active';
+$data['event_url'] = 'event/' . url_title($event_name);
 
 $this->load->view('view_includes/header.php', $data);
 $this->load->view('view_includes/view_question_pod.php');
