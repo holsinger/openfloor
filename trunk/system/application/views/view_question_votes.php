b6f644718b3b1057173b9da0639687b7e2c7d76f
daniel holsinger <holsinger@gmail.com>
2007-08-20 19:12:31 +0000

    add event url for submit a question link

--- trunk/system/application/views/view_question_votes.php
+++ trunk/system/application/views/view_question_votes.php
@@ -2,6 +2,7 @@
 $data['red_head'] = 'Votes';
 $data['tabs'] = $event_type;
 $data['tab_view_question'] = 'active';
+$data['event_url'] = 'event/'.$event_url_name;
 
 $this->load->view('view_includes/header.php',$data);
 $this->load->view('view_includes/view_question_pod');
