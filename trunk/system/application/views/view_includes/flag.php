f003c08b0b181c5b1449c89cf26cad99ea275c47
daniel holsinger <holsinger@gmail.com>
2007-09-04 23:20:27 +0000

    user avatar flag now has pointer cursor

--- trunk/system/application/views/view_includes/flag.php
+++ trunk/system/application/views/view_includes/flag.php
@@ -14,7 +14,7 @@ $html = $this->flag_lib->createFlagHTML($fk_id);
 ?>
 
 <? if($this->flag_lib->type == 'user'): ?>
-<img class="img-flag-<?=$this->flag_lib->type?>" src="./images/RP_Questions_flag.png" <? if(!empty($html)) echo "onclick=\"javascript:queueUpdater.toggleVisibility('flag_{$this->flag_lib->type}$fk_id');queueUpdater.toggleQueue();\"" ?>/>
+<img class="img-flag-<?=$this->flag_lib->type?>" style="cursor:pointer;" src="./images/RP_Questions_flag.png" <? if(!empty($html)) echo "onclick=\"javascript:queueUpdater.toggleVisibility('flag_{$this->flag_lib->type}$fk_id');queueUpdater.toggleQueue();\"" ?>/>
 <? endif; ?>
 <?=$html?>
 
