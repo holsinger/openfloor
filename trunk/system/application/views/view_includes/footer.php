b5880d31561574b4a0c0879a0c5ca71be591e971
daniel holsinger <holsinger@gmail.com>
2007-09-07 04:52:21 +0000

    rebrand

--- trunk/system/application/views/view_includes/footer.php
+++ trunk/system/application/views/view_includes/footer.php
@@ -5,7 +5,7 @@
     </div>
 		<!-- footer start here -->
 		<div id="footer">
-			<p>Copyright &copy; 2007Politic20.com. All Rights Reserved. Reproduction in whole or part in any form or medium without express written permission is strictly prohibited. </p>
+			<p>Copyright &copy; 2007RunPolitics.com. All Rights Reserved. Reproduction in whole or part in any form or medium without express written permission is strictly prohibited. </p>
 		</div>
 	</div></body>
 </html>
