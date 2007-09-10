6964ae7a9683980a824d2c6a2a54a13a3b58407f
daniel holsinger <holsinger@gmail.com>
2007-09-10 21:21:36 +0000

    link change

--- trunk/system/application/views/view_includes/view_right_events.php
+++ trunk/system/application/views/view_includes/view_right_events.php
@@ -3,7 +3,7 @@
 <h3><a class="close" onclick="new Effect.toggle('events','blind', {queue: 'end'}); ">close</a><span>Events</span></h3>
 <div class="box">
 <div class="inner-box" id="events">
- <span style="padding-left:5px;"><img src='./images/forum_logo.jpg'><br /><br />&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.politic20.com/index.php/conventionnext/queue/event/salt_lake_city_mayoral_forum" title="Salt Lake City Mayoral Forum"><strong>Aug 8, Salt Lake City Mayoral Forum</strong></a><br /><br /></span>
+ <span style="padding-left:5px;"><img src='./images/forum_logo.jpg'><br /><br />&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.runpolitics.com/index.php/conventionnext/queue/event/salt_lake_city_mayoral_forum" title="Salt Lake City Mayoral Forum"><strong>Aug 8, Salt Lake City Mayoral Forum</strong></a><br /><br /></span>
 </div>
 </div>
 <div class="box-bottom"></div> 
