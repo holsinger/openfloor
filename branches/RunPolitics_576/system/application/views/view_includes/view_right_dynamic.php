ccad992f87eb67308a2a9b6f55162f1588518891
daniel holsinger <holsinger@gmail.com>
2007-09-11 22:32:34 +0000

    fixed some layout issues with right dynamic pods

--- branches/RunPolitics_576/system/application/views/view_includes/view_right_dynamic.php
+++ branches/RunPolitics_576/system/application/views/view_includes/view_right_dynamic.php
@@ -4,7 +4,7 @@ foreach ($dynamic as $key => $data) {
 <h3><a class="close" onclick="new Effect.toggle('<?=$key;?>','blind', {queue: 'end'}); ">close</a><span><?=ucwords(str_replace('_',' ',$key));?></span></h3>
 <div class="box">
 <div class="inner-box" id="<?=$key;?>">
- <span style="padding-left:5px;"><?=$data;?></span>
+<?=$data;?>
 </div>
 </div>
 <div class="box-bottom"></div> 
