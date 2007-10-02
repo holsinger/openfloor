2098f88abf525581d017ff5106ad8eb4b979d9a8
daniel holsinger <holsinger@gmail.com>
2007-10-02 00:13:44 +0000

    add new admin functions

--- trunk/system/application/controllers/mainpage.php
+++ trunk/system/application/controllers/mainpage.php
@@ -5,9 +5,9 @@ class Mainpage extends Controller {
 	function __construct()
 	{
 		parent::Controller();
-		$this->load->model('Mainpage_model','mpDB');
+		$this->load->model('Mainpage_model','mainpage');
 		$this->load->library('validation');
-		$this->load->library('Mainpage');
+		$this->load->library('Mainpagelib');
 		$this->load->helper('url');//for redirect
 		$this->load->helper('form');
 
@@ -21,29 +21,68 @@ class Mainpage extends Controller {
 	}
 	
 	function view() {
-		$data['center'] = $this->mainpage->getCol(1,'html');
-		$data['right'] = $this->mainpage->getCol(2,'html'); 
+		$data['center'] = $this->mainpagelib->getCol(1,'html');
+		$data['right'] = $this->mainpagelib->getCol(2,'html'); 
 		$this->load->view('view_mainpage',$data);
 	}
 	
 	function admin() {
-		$data['center'] = $this->mainpage->getCol(1,'form');
-		$data['right'] = $this->mainpage->getCol(2,'form'); 
+		$this->userauth->check(SL_ADMIN);
+		
+		$data['center'] = $this->mainpagelib->getCol(1,'form');
+		$data['right'] = $this->mainpagelib->getCol(2,'form'); 
 		$this->load->view('view_mainpage',$data);
 	}
 	
 	function edit($rc,$ajax=false) {
 		//delete old pod
+		$this->delete($rc,true);
+		//get row & col
 		$col = substr($rc,0,1);
 		$row = substr($rc,1);
-		$this->mpDB->detele($col,$row);
 		//serialize data
 		$serialized = $_POST;
 		unset($serialized['delete']);
 		$serialized = serialize($serialized);
-		$this->mpDB->insert($col,$row,$serialized);
-		if ($ajax) return $this->mainpage->getHTML($serialized);
-		else $this->admin();
+		$this->mainpage->insert($col,$row,$serialized);
+		if ($ajax) return $this->mainpagelib->getHTML($serialized);
+		else {
+			redirect('mainpage/admin');
+			ob_clean();
+			exit();
+		}
+	}
+	
+	function add ($rc,$ajax=false) {
+		$col = substr($rc,0,1);
+		$row = substr($rc,1);
+		$data['name']='';
+		$data['feed']='';
+		$data['title']='';
+		$data['desc']='';
+		$data['desc_limit']='';
+		$data['items']='';
+		$serialized = serialize($data);
+		$this->mainpage->insert($col,$row,$serialized);
+		if ($ajax) return $this->mainpagelib->getForm($serialized,$rc);
+		else {
+			redirect('mainpage/admin');
+			ob_clean();
+			exit();
+		}
+	}
+	
+	function delete ($rc,$ajax=false) {
+		//delete old pod
+		$col = substr($rc,0,1);
+		$row = substr($rc,1);
+		$this->mainpage->delete($col,$row);
+		if ($ajax) return TRUE;
+		else {
+			redirect('mainpage/admin');
+			ob_clean();
+			exit();
+		}
 	}
 	
 }
