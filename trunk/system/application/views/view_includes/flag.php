0e27803aa4743dad9fdf0d5184866ad242b514a4
daniel holsinger <holsinger@gmail.com>
2007-10-04 21:05:05 +0000

    Fixed bug #23

--- trunk/system/application/views/view_includes/flag.php
+++ trunk/system/application/views/view_includes/flag.php
@@ -4,7 +4,7 @@ switch ($this->flag_lib->type) {
 		$fk_id = $question_id;
 		break;
 	case 'user':
-		$fk_id = $user_id;
+		$fk_id = $question_id;
 		break;
 	default:
 		show_error('flag.php::type: invalid type');
@@ -14,7 +14,11 @@ $html = $this->flag_lib->createFlagHTML($fk_id, $event_name);
 ?>
 
 <? if($this->flag_lib->type == 'user'): ?>
-<img class="img-flag-<?=$this->flag_lib->type?>" style="cursor:pointer;" src="./images/RP_Questions_flag.png" <? if(!empty($html)) echo "onclick=\"javascript:queueUpdater.toggleVisibility('flag_{$this->flag_lib->type}$fk_id');queueUpdater.toggleQueue();\"" ?>/>
+	<? if($this->userauth->isUser()): ?>
+		<img class="img-flag-<?=$this->flag_lib->type?>" style="cursor:pointer;" src="./images/RP_Questions_flag.png" <? if(!empty($html)) echo "onclick=\"javascript:queueUpdater.toggleVisibility('flag_{$this->flag_lib->type}$fk_id');queueUpdater.toggleQueue();\"" ?>/>
+	<? else: ?>
+		<img class="img-flag-<?=$this->flag_lib->type?>" style="cursor:pointer;" src="./images/RP_Questions_flag.png" onclick="showBox('login');" />
+	<? endif; ?>
 <? endif; ?>
 <?=$html?>
 
