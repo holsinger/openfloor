b394182e0d185db4629719c7f2405da19e228a28
daniel holsinger <holsinger@gmail.com>
2007-10-05 16:40:53 +0000

    Fixed ticket #31

--- trunk/system/application/views/question/question_view.php
+++ trunk/system/application/views/question/question_view.php
@@ -4,7 +4,12 @@ $data['tabs'] = $event_type;
 $data['tab_view_question'] = 'active';
 $data['event_url'] = 'event/' . url_title($event_name);
 
-$this->load->view('view_includes/header.php', $data);
+$this->load->view('view_includes/header.php', $data);?>
+<script type="text/javascript" charset="utf-8">
+	// Initialize, used in view_question_pod.php
+	queueUpdater.updateQueue();
+</script>
+<?
 $this->load->view('view_includes/view_question_pod.php');
 $this->load->view('view_includes/footer.php');
 ?>
