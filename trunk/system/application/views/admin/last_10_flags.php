fa5aacca539db1b01fbe2fe4d6435ca848a7b1f6
daniel holsinger <holsinger@gmail.com>
2007-10-05 15:55:19 +0000

    Tweaked question & flag view for admin dashboard

--- trunk/system/application/views/admin/last_10_flags.php
+++ trunk/system/application/views/admin/last_10_flags.php
@@ -1,6 +1,6 @@
 <table>
 	<th>Flag Type</th><th>What/Who</th><th>Flag</th><th>Reported By</th>
 	<? foreach($last_10_flags as $flag): ?>
-		<tr><td><?= $flag['type'] ?></td><td><?= $flag['object'] ?></td><td><?= $flag['flag'] ?></td><td><?= $flag['reporter'] ?></td></tr>
+	<tr><td><?= $flag['type'] ?></td><td><?= $flag['object'] ?></td><td><?= $flag['flag'] ?></td><td><?= $flag['reporter'] ?></td></tr>
 	<? endforeach; ?>
 </table>
