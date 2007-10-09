b962c42bbd2f189fc1c5db75d665f2ce28d5d2d2
daniel holsinger <holsinger@gmail.com>
2007-10-09 22:08:38 +0000

    fix abour us link

--- trunk/system/application/views/view_mainpage.php
+++ trunk/system/application/views/view_mainpage.php
@@ -35,7 +35,7 @@
                     <li class="separator">|</li>
                     <li><?=anchor("event/","Events");?></li>
                     <li class="separator">|</li>
-					<li><?=anchor("information/view/about_us","About Us");?></li>
+					<li><?=anchor("information/view/aboutUs","About Us");?></li>
                     <li class="separator">|</li>
 					<li><a href="http://blog.runpolitics.com">Blog</a></li>
                 </ul>
