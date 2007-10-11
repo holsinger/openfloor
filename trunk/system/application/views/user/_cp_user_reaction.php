9fb857299c9a6a6125b9541ab92eb1e811156555
daniel holsinger <holsinger@gmail.com>
2007-10-11 19:55:29 +0000

    avatars on dashboard

--- trunk/system/application/views/user/_cp_user_reaction.php
+++ trunk/system/application/views/user/_cp_user_reaction.php
@@ -3,7 +3,7 @@
 	<? $class = '' ?>
 	<? foreach($candidates as $v): ?>
 	<tr<?=$class?>>
-		<td><?=$v['can_display_name']?></td>
+		<td><?= $v['avatar'] . '<br/>' . $v['link_to_profile'] ?></td>
 		<td><div id="your-reaction-<?=$v['can_id']?>"><?$this->load->view('user/_userReactSlider', $v)?></div></td>
 	</tr>
 	<? $class = $class ? '' : ' class="alternate"' ?>
