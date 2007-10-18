b5660a8cba5589573326228c6f8805518e5646bf
daniel holsinger <holsinger@gmail.com>
2007-10-18 16:42:41 +0000

    the votes and comments count on the tabs overflow and you can't see them, please also put the number in front like the other question pod has it.

--- trunk/system/application/views/user/cp_current_question.php
+++ trunk/system/application/views/user/cp_current_question.php
@@ -22,8 +22,8 @@
 	<b class="question-pod2"><b></b></b>
 	<b class="question-pod1"><b></b></b></b>
 </div>
-<div class="votes" onClick="cpUpdater.viewVotes(<?= $current_question[0]['question_id'] ?>)">votes (<?= $current_question[0]['vote_count'] ?>)</div>
-<div class="comments" onClick="cpUpdater.viewComments(<?= $current_question[0]['question_id'] ?>, event_name, '<?= url_title($current_question[0]['question_name']) ?>')">comments (<?= $current_question[0]['comment_count'] ?>)</div>
+<div class="votes" onClick="cpUpdater.viewVotes(<?= $current_question[0]['question_id'] ?>)"><?= $current_question[0]['vote_count'] ?> votes</div>
+<div class="comments" onClick="cpUpdater.viewComments(<?= $current_question[0]['question_id'] ?>, event_name, '<?= url_title($current_question[0]['question_name']) ?>')"><?= $current_question[0]['comment_count'] ?> comments</div>
 <div style="clear:both;"></div>
 <? else: ?>
 There is no current question
