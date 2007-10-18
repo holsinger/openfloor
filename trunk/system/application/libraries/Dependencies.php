a4cf761ac31202fff9bf2a8f775972a23dcb0a11
daniel holsinger <holsinger@gmail.com>
2007-10-18 16:37:29 +0000

    Added dependencies class and JS/CSS optimizer

--- trunk/system/application/libraries/Dependencies.php
+++ trunk/system/application/libraries/Dependencies.php
@@ -1,55 +1,62 @@
 <?php
 
-/**
-* Dependencies class
-*/
 class Dependencies
 {
-	private $views_base_path = './system/application/views/';
-	private $views;
-	private $dependencies;
+	public $javascript;
+	public $css;
 	
+	private $views_base_path;
+	private $dependencies;
+		
 	function __construct()
 	{
-		// set the base path here (use system dir specified in global config)
-		$dependencies = array();
+		$this->CI =& get_instance();
+		$this->views_base_path = './system/application/views/';
+		$dependencies = $javascript = $css = array();
 	}
 	
-	public function func($view)
+	public function view($view)
 	{
-		$view = $this->check_extension($view);
-		$this->views = array($view);
-		
-		$this->populate_views($view);
-		echo('<pre>' . print_r($this->views, true) . '</pre>');
-		exit('<pre>' . print_r($this->dependencies, true) . '</pre>');
+		$view = $this->check_extension($view);		
+		$this->get_dependencies($view);
+		$this->sort_dependencies();
 	}
 	
-	private function populate_views($view)
+	private function get_dependencies($view)
 	{
-		$view_handle = fopen($this->views_base_path . $view, 'r');
+		// get contents of specified view
+		if(!$view_handle = @fopen($this->views_base_path . $view, 'r'))
+			show_error("Invalid view specified: $view");
 		$contents = fread($view_handle, filesize($this->views_base_path . $view));
-		preg_match_all('/\$this-\>load-\>view\([\'"](.*)[\'"]\)/', $contents, $views);
-		$this->views = array_merge($this->views, $views[1]);
 		
+		// find out if this views calls any other views
+		preg_match_all('/\$this-\>load-\>view\([\'"](.*)[\'"]\)/', $contents, $views);
+		
+		// populate dependencies array with any dependency tags found
 		preg_match_all('/#dependency (.*)/', $contents, $dependencies);
 		foreach($dependencies[1] as $dependency) $this->dependencies[] = $dependency;
 		
-		if(!empty($views[1]))			
-			foreach($views[1] as $v)
-				$this->populate_views($this->check_extension($v));
-		else
-			$this->views[] = $view;
+		// recursion: get dependencies for children views, if any
+		foreach($views[1] as $v) $this->get_dependencies($this->check_extension($v));
 		
 		fclose($view_handle);
 	}
 	
-	public function check_extension($view)
+	private function sort_dependencies()
+	{
+		foreach($this->dependencies as $dependency)
+			if(strpos(substr($dependency, -3), 'js') !== false) $this->javascript[] = $dependency;
+			elseif(strpos(substr($dependency, -3), 'css') !== false) $this->css[] = $dependency;
+			else show_error("Malformed dependency: $dependency");
+		$this->javascript = array_unique($this->javascript);
+		$this->css = array_unique($this->css);
+	}
+	
+	private function check_extension($view)
 	{
 		// make sure .php is appended to the string
-		return substr($view, 0, -4) != '.php' ? $view . '.php' : $view;
+		return substr($view, -4) != '.php' ? $view . '.php' : $view;
 	}
 }
-
 
 ?>
