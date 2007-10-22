7f9a7160d1266b6d8845a3872f1bf3e008aeab19
daniel holsinger <holsinger@gmail.com>
2007-10-22 17:30:28 +0000

    Changed image size of avatar

--- trunk/system/application/controllers/forums.php
+++ trunk/system/application/controllers/forums.php
@@ -837,7 +837,7 @@ EOT;
 		foreach($data['candidates'] as $k => $v) {
 			$data['candidates'][$k]['user_reaction'] = $this->reaction->canUserReaction($v['can_id']);
 			$data['candidates'][$k]['link_to_profile'] = $this->candidate->linkToProfile($v['can_id'], false, true);
-			$data['candidates'][$k]['avatar'] = '<a href="' . $this->candidate->linkToProfile($v['can_id'], true) . '"><img src="./avatars/'.$this->candidate->canAvatar($v['can_id']).'"/></a>';
+			$data['candidates'][$k]['avatar'] = '<a href="' . $this->candidate->linkToProfile($v['can_id'], true) . '"><img src="./avatars/shrink.php?img='.$this->candidate->canAvatar($v['can_id']).'&w=16&h=16"/></a>';
 		}
 	}
 	
