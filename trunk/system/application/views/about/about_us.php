e4aa7c944e0f8ad79b6333ffbec88bea460f9704
daniel holsinger <holsinger@gmail.com>
2007-10-24 21:08:04 +0000

    Added arrows to the side.  After some ugly IE 6 hacking this finally works on all browser versions.  Not my proudest moment...

--- trunk/system/application/views/about/about_us.php
+++ trunk/system/application/views/about/about_us.php
@@ -5,27 +5,42 @@
 	
 <?php $this->load->view('view_includes/header.php',$data); ?>
 <div id="content_div">
-            <h3>About Us</h3>
-			<div id="top_about_us">
-				<table border="0" cellspacing="0" cellpadding="0" width="100%">
-					<tr><td align="center"><img class="link" onclick="MoveBackwards();"  src="./images/about/RP_AboutUs_BackArrow.png" border="0" title="Move Backward"><img class="link" onclick="MoveForward();" src="./images/about/RP_AboutUs_ForwardArrow.png" border="0" title="Move Forward"></td></tr>
-				</table>
-			</div>
-			<div  id="outer_about_div" style="position: relative; overflow: hidden; width: 97%; height: 700px; border: 0px solid #000000; z-index: 3;">
-	            <div id="inner_about_div" style="position: relative; height: 344px; width: 4000px; z-index: 2;">
-					<? foreach($info as $name => $value): ?>
-						<div id="<?=$name?>_div" style="float:left; width: <?=$info[$name]['img_size']?>px; overflow: visible;">
-							<img id="<?=$name?>" src="./images/about/<?=$name?>_off.jpg" border="0" onmouseover="ChangeAboutImage(this, 'on');" onmouseout="ChangeAboutImage(this, 'off');">
+        <h3>About Us</h3>
+		<div id="loading_div" style="position: relative; font-size: 2em; text-align: center;">LOADING...</div>
+		<div style="position: relative; overflow: hidden; width: 100%">
+			<table cellpadding="0" cellspacing="0" border="0">
+				<tr>
+					<td width="43" valign="top">
+						<div style="margin-top: 130px">
+							<img class="link" onclick="MoveBackwards();"  src="./images/about/RP_AboutUs_BackArrow.png" border="0" title="Move Backward">
 						</div>
-					<? endforeach; ?>
-	            </div>
-	
-				<div style="visibility: hidden; width: 250px; top: 350px; position: absolute; margin-top: 10px" id="about_bio"> 
-					<div style="font-weight: bold;" id="bio_name"></div>
-					<div id="bio_content"></div>
-				</div>	
-			</div>
+					</td>
+					<td width="100%" id="main_content_td">
+						<div  id="outer_about_div" style="position: relative; overflow: hidden; width: 0px; height: 700px; border: 0px solid #000000; z-index: 3;">
+		            		<div id="inner_about_div" style="position: relative; height: 344px; width: 4000px; z-index: 2;">
+								<? foreach($info as $name => $value): ?>
+									<div id="<?=$name?>_div" style="float:left; width: <?=$info[$name]['img_size']?>px; overflow: visible;">
+										<img id="<?=$name?>" src="./images/about/<?=$name?>_off.jpg" border="0" onmouseover="ChangeAboutImage(this, 'on');" onmouseout="ChangeAboutImage(this, 'off');">
+									</div>
+								<? endforeach; ?>
+		            		</div>
 
+						</div>
+					</td>
+					<td width="43" valign="top">
+						<div style="margin-top: 130px">
+							<img class="link" onclick="MoveForward();" src="./images/about/RP_AboutUs_ForwardArrow.png" border="0" title="Move Forward">
+						</div>
+					</td>
+				</tr>
+			</table>
+			<!-- Moving Bio Div -->
+			<div style="visibility: hidden; width: 250px; top: 350px; position: absolute; margin-top: 10px" id="about_bio"> 
+				<div style="font-weight: bold;" id="bio_name"></div>
+				<div id="bio_content"></div>
+			</div>
+		</div>
+		
 </div>
 <script type="text/javascript" charset="utf-8">
 	var info = <?=json_encode($info);?>
@@ -44,7 +59,7 @@
 			$('bio_content').innerHTML = eval('info.'+elem.id+'.bio;')+'<? if ($this->userauth->isSuperAdmin()) echo "<br>".anchor("admin/cms/about_us_'+elem.id+'", 'edit'); ?>';
 
 			$('about_bio').setStyle({
-				left: (div_pos[0] + about_bio_offset)+"px"
+				left: (div_pos[0] + about_bio_offset)+43+"px" // 43 is the size of the arrow image
 			});
 			
 		}
@@ -91,5 +106,13 @@
 		}
 
 	}	
+	// Ugly IE 6 Junk
+	Event.observe(window, "load", onLoad);
+	function onLoad(){
+		$('outer_about_div').setStyle({
+			width: $('main_content_td').offsetWidth+"px"
+		});
+		$('loading_div').innerHTML = '';
+	}
 </script>
 <? $this->load->view('view_includes/footer.php'); ?>
