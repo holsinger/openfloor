82c7d5426136949e4470faade0baa6e64156ff96
daniel holsinger <holsinger@gmail.com>
2007-10-26 20:30:05 +0000

    Change

--- trunk/system/application/views/view_events.php
+++ trunk/system/application/views/view_events.php
@@ -48,7 +48,7 @@ $data['top_banner_text'] = "OPENFLOOR EVENTS";
 		<? endif; ?>
 	<? endforeach; ?>
 	<? if($count < 1): ?>
-		<img src="images/RP_OpenFloorEvents.png"/>
+		<img src="./images/RP_OpenFloorEvents.png"/>
 		<div style="margin-left:10px;margin-right:30px;">
 			<div style="float: left; margin: 0px 5px 5px 0px;">
 				<div><img src="./images/events/event_example.jpg" border="0" /></div>
