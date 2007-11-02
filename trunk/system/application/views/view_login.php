21929872a3a83951682bc2fb7ba030f35b77c1a5
daniel holsinger <holsinger@gmail.com>
2007-11-02 14:39:34 +0000

    Added new create account for this page.

--- trunk/system/application/views/view_login.php
+++ trunk/system/application/views/view_login.php
@@ -6,6 +6,7 @@
   	<div class='errorArea'><?=$error;?></div>
   	<br />
 	<div id="login_form">
+	Don't have a login? <?=anchor('user/createAccount','<b>Create an Account</b>');?>.<br /><br />
 	<?= form_open('user/login'); ?>
 	<?= form_format("Username: ",form_input('user_name',(isset($this->validation->user_name)) ? $this->validation->user_name:'','class="txt"') ); ?>
 	<?= form_format("Password: ",form_password('user_password','','class="txt"') ); ?>
