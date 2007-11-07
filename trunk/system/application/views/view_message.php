6f21e96a0ae7e3d417e9de55644a9c509f31620e
daniel holsinger <holsinger@gmail.com>
2007-11-07 23:09:34 +0000

    Last minute cleanup

--- trunk/system/application/views/view_message.php
+++ trunk/system/application/views/view_message.php
@@ -3,10 +3,10 @@ $data['sub_title'] = $cms_name;
 $this->load->view('view_includes/header.php',$data); ?>
 
 <div id="content_div">
-            <div style="margin-left:10px;margin-right:30px;">
-            <?=$cms_text;?>
-            </div>
-			<br /><br />
-            <? if ($this->userauth->isSuperAdmin()) echo "<div>".anchor('admin/cms/'.$cms_url, 'edit')."</div>"; ?>
+	<div style="margin-left:10px;margin-right:30px;">
+	<?=$cms_text;?>
+	</div>
+	<br /><br />
+	<? if ($this->userauth->isSuperAdmin()) echo "<div>".anchor('admin/cms/'.$cms_url, 'edit')."</div>"; ?>
 </div>
 <? $this->load->view('view_includes/footer.php'); ?>  				
