ba6df0e8e32f78663cb912c92cfd1b74229f0016
daniel holsinger <holsinger@gmail.com>
2007-11-13 20:31:15 +0000

    A nice overhaul which gets past the nasty slowness of IE.

--- trunk/javascript/popup.js
+++ trunk/javascript/popup.js
@@ -9,13 +9,15 @@ Control.PopUp = Class.create();
 Control.PopUp.prototype = {
 	initialize: function (src_elem_id, options){
 		this.src_elem_id = src_elem_id;
-		this.popup_div = false;		// Don't have it created right now
+		this.popup_div = "";
+		
 		// We have a lot of defaults that we use if not defined
 		this.options = Object.extend({
 			src_elem_event : 'mousedown',
 			popup_placement : 'below',		// Can be "below" or "page_center"
 			hijax: 'false', 
 			hijax_div_class: 'hijax',
+			popup_div_class: 'popup',
 			popup_class: 'popup_class',
 			document_hide_event: 'true',
 			ajax_update_url: 'none',
@@ -39,12 +41,6 @@ Control.PopUp.prototype = {
 			}
 			
 		}
-		// If page_center and a modern browser, set popup_elem_id to be fixed
-		if (this.options.popup_placement == "page_center") {			
-		  	$(this.popup_elem_id).setStyle({
-				position: 'fixed'
-			});
-		}
 		// Assign Events	
 		this.popup_elem_event = this.onPopupElemMouseDown.bindAsEventListener(this);
 		if(this.options.src_elem_event == 'mousedown'){
@@ -57,6 +53,9 @@ Control.PopUp.prototype = {
 		}else if(this.options.src_elem_event == 'mouseover'){
 			// TODO : add support for mouseover 
 		}
+		
+		// Finally, create the first popup div
+		Event.observe(window, 'load', this.onWindowLoad.bind(this))
 	}, 
 	getClientDim : function(){
 		var client_width;
@@ -79,8 +78,7 @@ Control.PopUp.prototype = {
 			return [div_dim[0], top];
 		}else if(this.options.popup_placement == 'page_center'){
 			var client_dim = this.getClientDim();
-
-			return [((client_dim[0] / 2) - ($(this.popup_elem_id).getWidth() / 2)), ((client_dim[1] / 2) - ($(this.popup_elem_id).getHeight() / 2))];
+			return [((client_dim[0] / 2) - ($(this.popup_div).getWidth() / 2)), ((client_dim[1] / 2) - ($(this.popup_div).getHeight() / 2))];
 		}else{
 			alert('Invalid "popup_palcement" value provided in PopUp definition!')
 		}
@@ -88,10 +86,10 @@ Control.PopUp.prototype = {
 	onSrcElemMouseDown : function(event){
 		this.cancel_document_event = true;
 		// When using mousedown we want the abilit to toggle
-		if(this.popup_div){
-			Event.stopObserving($(this.popup_div), 'mousedown', this.popup_elem_event);
-			$(this.popup_div).remove();
-			this.popup_div = false;
+		if(this.popup_div.getStyle('visibility') == 'visible'){
+			// Remove
+			this.destroy()
+			
 		}else{
 			// If Hijax is set then show it
 			if(this.options.hijax == 'true'){
@@ -104,19 +102,20 @@ Control.PopUp.prototype = {
 					visibility: "visible"
 				});
 			}
+			new Ajax.Updater($(this.popup_div), this.options.ajax_update_url, { method: 'get' });
 			// Positioning
 			var pos = this.showPosition(this.src_elem_id);
 			// Set Style
-			this.popup_div = document.createElement('div');
-			document.body.appendChild(this.popup_div);
-			$(this.popup_div).addClassName(this.options.hijax_div_class);
+			//console.log('Showing!');
 			$(this.popup_div).setStyle({
 				position: "absolute",
+				visibility: "visible",
+				display: "block",
 				left: pos[0]+this.options.offset_x+"px",
 				top: pos[1]+this.options.offset_y+"px"
 			});
-			Event.observe($(this.popup_div), 'mousedown', this.popup_elem_event);
-			new Ajax.Updater($(this.popup_div), this.options.ajax_update_url, { method: 'get' });
+			
+			
 		}
 
 		
@@ -130,11 +129,9 @@ Control.PopUp.prototype = {
 					visibility: "hidden"
 				});
 			}
-			// Set Style
-			$(this.popup_elem_id).setStyle({
-				visibility: 'hidden',
-				display: 'none'
-			});
+			// Remove and recreate popup
+			this.destroy();
+			
 		}else{
 			this.cancel_document_event = false;		// reset
 		}
@@ -143,9 +140,15 @@ Control.PopUp.prototype = {
 		this.cancel_document_event = true;
 	},
 	destroy : function(){
-		Event.stopObserving($(this.popup_div), 'mousedown', this.popup_elem_event);
-		$(this.popup_div).remove();
-		this.popup_div = false;
+		$(this.popup_div).setStyle({ visibility: 'hidden', display: 'none' });
+		$(this.popup_div).innerHTML = '';
+	},
+	onWindowLoad : function(){
+		this.popup_div = document.createElement('div');
+		document.body.appendChild(this.popup_div);
+		$(this.popup_div).addClassName(this.options.popup_div_class);
+		$(this.popup_div).setStyle({ visibility: 'hidden', display: 'none' });
+		Event.observe($(this.popup_div), 'mousedown', this.popup_elem_event);
 	}
 
 }
