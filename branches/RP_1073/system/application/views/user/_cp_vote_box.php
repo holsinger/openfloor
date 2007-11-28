9e22840c1b562bbf9e7257b5519abf89124731d2
daniel holsinger <holsinger@gmail.com>
2007-11-28 22:32:10 +0000

    voted checkbox fix

--- branches/RP_1073/system/application/views/user/_cp_vote_box.php
+++ branches/RP_1073/system/application/views/user/_cp_vote_box.php
@@ -1,6 +1,7 @@
 <? if(!$this->userauth->isUser()): ?>
 	<img src ="./images/ucp/vote-up.jpg" alt="Vote Up" title="Vote Up" onclick="showBox('login')">
 <? elseif ($voted > 0): ?>
+	<img src="./images/votedCheckBoxBG.png"/>
 	<div class='checkbox'></div>
 <? else: ?>
 	<img src="./images/ucp/vote-up.jpg" alt="Vote Up" title="Vote Up" onClick="javascript:cpUpdater.vote(site_url + 'question/voteup/event/<?= $event_url_name; ?>/question/<?= url_title($question_name); ?>');"/>
@@ -9,7 +10,7 @@
 <? if(!$this->userauth->isUser()): ?>
 	<img src ="./images/ucp/vote-down.jpg" alt="Vote Down" title="Vote Down" onclick="showBox('login')">
 <? elseif ($voted < 0): ?>
-	<div class='checkbox'></div>		
+	<img src="./images/votedCheckBoxBG.png"/>		
 <? else: ?>
 	<img src="./images/ucp/vote-down.jpg" alt="Vote Down" title="Vote Down" onClick="javascript:cpUpdater.vote(site_url + 'question/votedown/event/<?= $event_url_name; ?>/question/<?= url_title($question_name); ?>');"/>
 <? endif; ?>
