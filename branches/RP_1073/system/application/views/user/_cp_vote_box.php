4ae133f78926d0c98306c56e108e54f414e1badc
daniel holsinger <holsinger@gmail.com>
2007-11-28 20:54:09 +0000

    small link and style changes to cp question pod

--- branches/RP_1073/system/application/views/user/_cp_vote_box.php
+++ branches/RP_1073/system/application/views/user/_cp_vote_box.php
@@ -1,7 +1,7 @@
 <? if(!$this->userauth->isUser()): ?>
 	<img src ="./images/ucp/vote-up.jpg" alt="Vote Up" title="Vote Up" onclick="showBox('login')">
 <? elseif ($voted > 0): ?>
-	<img src="./images/votedCheckBox.png"/>
+	<div class='checkbox'></div>
 <? else: ?>
 	<img src="./images/ucp/vote-up.jpg" alt="Vote Up" title="Vote Up" onClick="javascript:cpUpdater.vote(site_url + 'question/voteup/event/<?= $event_url_name; ?>/question/<?= url_title($question_name); ?>');"/>
 <? endif; ?>
@@ -9,7 +9,7 @@
 <? if(!$this->userauth->isUser()): ?>
 	<img src ="./images/ucp/vote-down.jpg" alt="Vote Down" title="Vote Down" onclick="showBox('login')">
 <? elseif ($voted < 0): ?>
-	<img src="./images/votedCheckBox.png"/>		
+	<div class='checkbox'></div>		
 <? else: ?>
 	<img src="./images/ucp/vote-down.jpg" alt="Vote Down" title="Vote Down" onClick="javascript:cpUpdater.vote(site_url + 'question/votedown/event/<?= $event_url_name; ?>/question/<?= url_title($question_name); ?>');"/>
 <? endif; ?>
