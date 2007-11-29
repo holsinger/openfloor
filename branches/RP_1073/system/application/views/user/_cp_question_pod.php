e75c8ea20f3edb32ff95d0b669c8e66ded986393
daniel holsinger <holsinger@gmail.com>
2007-11-29 22:20:44 +0000

    question score TT

--- branches/RP_1073/system/application/views/user/_cp_question_pod.php
+++ branches/RP_1073/system/application/views/user/_cp_question_pod.php
@@ -18,7 +18,7 @@ if(isset($current_question_flag)) $question = $current_question;
 		  <div class="question-podfg">
 			<table cellpadding="0" cellspacing="0">
 				<tr>
-					<td><div class="score"><?= $question['votes'] ?></div></td>
+					<td><div class="score" title='Question Score'><?= $question['votes'] ?></div></td>
 					<td><div class="vote"><? $this->load->view('user/_cp_vote_box', $question) ?></div></td>
 					<td width="100%"><div class="question"<?= isset($current_question_flag) ? ' id="the-current-question"' : '' ?>><a href="<?= $this->config->site_url();?>/user/profile/<?=$question['user_name'];?>"><img class="sc_image" src="./avatars/shrink.php?img=<?=$question['avatar_path'];?>&w=16&h=16"></a>&nbsp;<?= $question['question_name'] ?></div></td>
 					<td><div class="flag"><!-- <img src="./images/flag.png"> --></div></td>
