65ae8011999203673ed44104e4e6d2a0fd626103
daniel holsinger <holsinger@gmail.com>
2007-12-03 19:03:53 +0000

    initial splah page

--- branches/RP_1073/system/application/controllers/splash.php
+++ branches/RP_1073/system/application/controllers/splash.php
@@ -1,20 +1,35 @@
 <?
 
-class Mainpage extends Controller {
+class Splash extends Controller {
 
 	function __construct()
 	{
 		parent::Controller();
+		
+		$this->load->model('Cms_model','cms');
 
 	}
 	
 	
 	function index()
 	{		
-		$this->view();
+		$this->failSafe();
 	}
 	
-	function view() { 
+	function failSafe()
+	{
+		$this->view('home');
+	}
+	
+	function view ($cms_string)
+	{
+		$data['cms_id'] = 0;
+		if ( is_string($cms_string) ) {
+			$data['cms_id'] = $this->cms->get_id_from_url($cms_string);
+			if ($data['cms_id']>0) 
+				$data = $this->cms->get_cms($data['cms_id']);	
+		}
+		
 		$this->load->view('view_splash',$data);
 	}
 	
