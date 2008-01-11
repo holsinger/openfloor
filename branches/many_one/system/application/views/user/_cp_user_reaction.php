fb8abb2e05c46a94adc6a87e0510fb3489c7dbe3
daniel holsinger <holsinger@gmail.com>
2008-01-11 23:07:30 +0000

    More changes.  New thumb images that work better with their color scheme.

--- branches/many_one/system/application/views/user/_cp_user_reaction.php
+++ branches/many_one/system/application/views/user/_cp_user_reaction.php
@@ -1,14 +1,14 @@
 <table>
 	<tr>
-		<th class="candidate">&nbsp;&nbsp;Speaker</th>
-		<th style="background-color:#3561b7;">Your Reaction</th>
+		<th class="candidate" width="120">&nbsp;&nbsp;Speaker</th>
+		<th width="150" style="background-color:#3561b7;">Your Reaction</th>
 		<th class="reaction">Overall&nbsp;&nbsp;</th>
 	</tr>
 	<? $class = '' ?>
 	<? foreach($candidates as $v): ?>
 		<tr<?=$class?>>
-			<td><?= $v['avatar'] . '&nbsp;' . $v['link_to_profile'] ?></td>
-			<td><div id="your-reaction-<?=$v['user_id']?>"><?$this->load->view('user/_userReactSlider', $v)?></div></td>
+			<td width="120"><?= $v['avatar'] . '&nbsp;' . $v['link_to_profile'] ?></td>
+			<td width="150"><div id="your-reaction-<?=$v['user_id']?>"><?$this->load->view('user/_userReactSlider', $v)?></div></td>
 			<td><div id="overall-reaction-<?=$v['user_id']?>"><?$this->load->view('user/_overallReaction', $v)?></div></td>
 		</tr>
 	<? $class = $class ? '' : ' class="alternate"' ?>
