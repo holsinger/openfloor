f64733dd60165ab69213abae3a52ac2795d680e8
daniel holsinger <holsinger@gmail.com>
2008-02-04 19:59:28 +0000

    Requires login before they can provide question answered feedback

--- branches/many_one/system/application/views/event/ajax_participant.php
+++ branches/many_one/system/application/views/event/ajax_participant.php
@@ -1,12 +1,12 @@
 <? if($respondent['status'] == 'responding'): ?>
 	<? if($past_vote < 1): ?>
 		<small>Let the respondent know that you don't feel that he/she is answering the question by clicking on the button below.</small><br /><br />
-		<a href="javascript: var none = cpUpdater.ajaxParticipantVote('up');">
+		<a href="javascript: <?= $this->userauth->isUser() ? 'var none = cpUpdater.ajaxParticipantVote(\'up\');' : 'window.location=\''.$this->config->item('m1_url').'/login/\';' ?>">
 			<img src="images/ucp/not_answered_button.png" border="0" />
 		</a>
 	<? else: ?>
 			<small>Let the respondent know that you don't feel that he/she is answering the question by clicking on the button below.</small><br /><br />
-			<a href="javascript: var none = cpUpdater.ajaxParticipantVote('down');">
+			<a href="javascript: <?= $this->userauth->isUser() ? 'var none = cpUpdater.ajaxParticipantVote(\'down\');' : 'window.location=\''.$this->config->item('m1_url').'/login/\';' ?>">
 				<img src="images/ucp/answered_button.png" border="0" />
 			</a>
 	<? endif; ?>
